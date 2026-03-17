b9dd7259f29bf340ff7ef72edde09b4c817bf003
Lukas Neumann <lneumann@rahmenwerk.media>
2026-03-17 11:26:47 +0100

    chore: update @tiptap dependencies to version 3.0.7 in package.json and package-lock.json, and remove unused static assets from the backend

--- vite.config.ts
+++ vite.config.ts
@@ -20,30 +20,12 @@ export default defineConfig({
         APP_BUILD_HASH: JSON.stringify(process.env.APP_BUILD_HASH || 'dev-build')
     },
     build: {
-        sourcemap: true,
-        commonjsOptions: {
-            include: [/@tiptap/, /node_modules/]
-        }
+        sourcemap: true
     },
     worker: {
         format: 'es'
     },
     esbuild: {
         pure: process.env.ENV === 'dev' ? [] : ['console.log', 'console.debug', 'console.error']
-    },
-    // HIER IST DER NEUE SSR-BLOCK
-    ssr: {
-        noExternal: [/^@tiptap\//, /^prosemirror-/]
-    },
-    optimizeDeps: {
-        include: [
-            '@tiptap/core',
-            '@tiptap/pm',
-            '@tiptap/starter-kit',
-            'prosemirror-state',
-            'prosemirror-transform',
-            'prosemirror-model',
-            'prosemirror-view'
-        ]
     }
 });
